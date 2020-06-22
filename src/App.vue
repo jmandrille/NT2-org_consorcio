525f399820f8e6af22c0c710707fe872f3fb636b
Javier Mandrille <55599058+jmandrille@users.noreply.github.com>
2020-06-22 12:13:12 -0300

    pasaje del sidebar al header / funciones al board task

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,101 @@
 <template>
   <div id="app" class="container col-12">
-      <div class="row">
-        <nav id="header" class="navbar navbar-expand-lg navbar-dark bg-dark col-12">
-          <div class="container col-12">
-            <a class="navbar-brand" href="#">
-              <img width="45px" height="45px" src="./assets/logo2.png" alt="tc4" />
-              TheConsortium4
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbar">
-              <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                  <router-link class="nav-link" to="/usuario">Usuario</router-link>
-                </li>
-                <li class="nav-item">
-                  <router-link class="nav-link" to="/encargado">Encargado</router-link>
-                </li>
-              </ul>
-            </div>
+    <div class="row">
+      <nav
+        id="header"
+        class="navbar navbar-expand-lg navbar-dark bg-dark col-12"
+      >
+        <div class="container col-12">
+          <a class="navbar-brand" href="#">
+            <img
+              width="45px"
+              height="45px"
+              src="./assets/logo2.png"
+              alt="tc4"
+            />
+            TheConsortium4
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbar"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/">Home</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/reporte"
+                  >Reporte</router-link
+                >
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/historico"
+                  >Historico</router-link
+                >
+              </li>
+            </ul>
           </div>
-        </nav>
-      </div>
-      <div class="row">
-        <div class="col-2 row p-2 bg-dark navbar-fixed-left">
-            <router-link   to="/" class="col-12">
-              <b-button block pill class="p-2 m-1" variant="primary">Home</b-button>
-            </router-link>
-            <div class="col-12">
-              <b-button v-b-modal.modal-1 block pill class="col-12 p-2 m-1" variant="primary">Agregar tarea</b-button>
-              <b-modal id="modal-1" title="Nueva tarea"></b-modal>
-            </div>
-            <router-link to="/reporte" class="col-12">
-              <b-button block pill class="p-2 m-1" variant="primary">Generar reporte</b-button>
-            </router-link>
-            <router-link to="/historico" class="col-12">
-              <b-button block pill class="p-2 m-1" variant="primary">Historico</b-button>
-            </router-link>
-        </div>
-        <div class="col-10">
-          <router-view />
-        </div>
-      </div>
-      <div class="row">
-        <footer id="footer" class="pb-4 pt-4">
-          <div class="container fixed-bottom">
-            <div class="text-center align-items-center">
-              <h6>© TheConsortium4 - 2020</h6>
-              <small>- Todos los derechos reservados - </small>
-            </div>
+          <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/usuario"
+                  >Usuario</router-link
+                >
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/encargado"
+                  >Encargado</router-link
+                >
+              </li>
+            </ul>
           </div>
-        </footer>
-      </div>
+        </div>
+      </nav>
+    </div>
+    <div class="col-12">
+      <router-view />
+    </div>
+    <div class="row">
+      <footer id="footer" class="pb-4 pt-4">
+        <div class="container fixed-bottom">
+          <div class="text-center align-items-center">
+            <h6>© TheConsortium4 - 2020</h6>
+            <small>- Todos los derechos reservados -</small>
+          </div>
+        </div>
+      </footer>
+    </div>
   </div>
 </template>
 
-
 <script>
-
 export default {
   name: "App",
-  components: {},  
+  components: {},
   data() {
     return {
-        isReporteActive: this.$router.currentRoute.path == '/reporte',
-        isHistoricoActive: this.$router.currentRoute.path == '/historico',
-    }
+      isReporteActive: this.$router.currentRoute.path == "/reporte",
+      isHistoricoActive: this.$router.currentRoute.path == "/historico"
+    };
   },
   methods: {
-    toggleColors: function(active, path){
+    toggleColors: function(active, path) {
       console.log(active);
       console.log(path);
-      console.log(this.$router.currentRoute.path == '/' + path);
-      return active = this.$router.currentRoute.path == '/' + path;
+      console.log(this.$router.currentRoute.path == "/" + path);
+      return (active = this.$router.currentRoute.path == "/" + path);
     }
   }
 };
 </script>
-
 
 <style>
 #app {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,128 @@
 <template>
   <div id="home" class="container col-12">
     <div class="row">
+      <nav
+        id="header"
+        class="navbar navbar-expand-lg navbar-light col-12"
+        style="background-color: #FFFFFF;"
+      >
+        <div class="container col-12">
+          <div class="form-inline">
+            <b-form-input
+              id="input-2"
+              v-model="nuevoItem"
+              required
+              placeholder="Ingrese tarea aquí"
+              @keyup.enter="agregarItem"
+            ></b-form-input>
+            <b-button @click="agregarItem" variant="outline-info" class="ml-2"
+              >Agregar</b-button
+            >
+          </div>
+          <form class="form-inline">
+            <input
+              class="form-control mr-sm-2"
+              type="search"
+              placeholder="Nombre Tarea"
+              aria-label="Search"
+              required
+            />
+            <button class="btn btn-outline-info my-2 my-sm-0" type="submit">
+              Buscar Tarea
+            </button>
+          </form>
+          <b-button
+            v-b-modal.modal-1-prevent-closing
+            class="p-2 m-1"
+            type="submit"
+            variant="outline-info"
+            @click="add"
+            >Agregar Tarea</b-button
+          >
+          <b-modal
+            id="modal-1-prevent-closing"
+            ref="modal"
+            title="Nueva tarea"
+            variant="secondary"
+            @show="resetModal"
+            @hidden="resetModal"
+            @ok="agregarItem"
+          >
+            <form ref="form" @submit.stop.prevent="agregarItem">
+              <b-form-group
+                :state="nameState"
+                label="Título tarea"
+                label-for="name-input"
+                invalid-feedback="Título es requerido"
+              >
+                <b-form-input
+                  id="name-input"
+                  v-model="name"
+                  :state="nameState"
+                  required
+                ></b-form-input>
+              </b-form-group>
+              <b-form-group
+                :state="descripcionState"
+                label="Descripción"
+                label-for="descripcion-input"
+                invalid-feedback="Descripción es requerido"
+              >
+                <b-form-input
+                  id="descripcion-input"
+                  v-model="descripcion"
+                  :state="descripcionState"
+                  required
+                ></b-form-input>
+              </b-form-group>
+              <b-form-group
+                label="Categoría"
+                label-for="categoria-input"
+                invalid-feedback="Categoría es requerido"
+              >
+                <b-form-select
+                  v-model="selected"
+                  :options="options"
+                ></b-form-select>
+              </b-form-group>
+            </form>
+          </b-modal>
+
+          <b-dropdown
+            block
+            class="p-2 m-1"
+            @click="add"
+            text="Filtrar por Categorías"
+            variant="outline-info"
+          >
+            <b-dropdown-item variant="primary">Mantenimiento</b-dropdown-item>
+            <b-dropdown-divider></b-dropdown-divider>
+            <b-dropdown-item variant="success">Seguridad</b-dropdown-item>
+            <b-dropdown-divider></b-dropdown-divider>
+            <b-dropdown-item variant="warning">Limpieza</b-dropdown-item>
+          </b-dropdown>
+        </div>
+      </nav>
+    </div>
+    <div class="row">
       <div class="col-4 mt-5">
-        <div class="p-2 alert alert-secondary ">
+        <div class="p-2 alert alert-secondary">
           <h3 class="text-center">Solicitado</h3>
           <hr />
           <!-- lista items solicitados -->
           <draggable
             class="list-group kanban-column"
             :list="listaItems"
-            group="tasks">
+            group="tasks"
+          >
             <div
               class="list-group-item"
               v-for="element in listaItems"
-              :key="element.name">
+              :key="element.name"
+            >
               {{ element.name }}
             </div>
           </draggable>
-          <hr />
-          <div class="row justify-content-center">
-            <div class="form-inline">
-              <b-form-input
-                id="input-2"
-                v-model="nuevoItem"
-                required
-                placeholder="Ingrese tarea aquí"
-                @keyup.enter="agregarItem"
-              >
-              </b-form-input>
-              <b-button @click="agregarItem" variant="primary" class="ml-2"
-                >+</b-button
-              >
-            </div>
-          </div>
         </div>
       </div>
 
@@ -88,13 +178,24 @@ export default {
   name: "Home",
   components: {
     draggable
-  },  
+  },
   data() {
     return {
       // nueva item
       nuevoItem: "",
+      //categorias
+      selected: null,
+      options: [
+        { value: "limpieza", text: "Limpieza" },
+        { value: "seguridad", text: "Seguridad" },
+        { value: "mantenimiento", text: "Mantenimiento" }
+      ],
       // lista de items
-      listaItems: [{ name: "Vayanse" }, { name: "a" }, { name: "la" }],
+      listaItems: [
+        { name: "Tarea 1" },
+        { name: "Tarea 2" },
+        { name: "Tarea 3" }
+      ],
       itemsEnProceso: [],
       itemsFinalizados: []
     };
@@ -108,7 +209,7 @@ export default {
       }
     },
     irAHistorico: function() {
-      this.$router.push({name:'Historico'}) 
+      this.$router.push({ name: "Historico" });
     }
   }
 };
